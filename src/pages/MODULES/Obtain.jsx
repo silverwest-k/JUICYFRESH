3beb5053150307e97df2d0ac1a67c2cdd4ff5e3f
Eunseo <supreme327@naver.com>
2023-05-25 10:13:02 +0900

    수주고나리 일부수정

--- src/pages/MODULES/Obtain.jsx
+++ src/pages/MODULES/Obtain.jsx
@@ -1,26 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button} from "react-bootstrap";
 import Table from "react-bootstrap/Table";
-// import { saveAs } from 'file-saver';
-// import XLSX from 'xlsx';
+import {useEffect, useState} from "react";
+import { saveAs } from 'file-saver';
+import * as XLSX from "xlsx";
 
 // 수주관리
 function Obtain() {
 
-  // // 엑셀 다운받기 함수
-  //   const downloadExcel = () => {
-  //       const wb = XLSX.utils.book_new();
-  //       const ws = XLSX.utils.json_to_sheet(data);
-  //
-  //       XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
-  //       const excelBuffer = XLSX.write(wb, {
-  //           bookType: "xlsx",
-  //           type: "array",
-  //       });
-  //
-  //       const dataBlob = new Blob([excelBuffer], { type: "application/octet-stream" });
-  //       saveAs(dataBlob, `data_${Date.now()}.xlsx`);
-  //   };
+    const [data, setData] = useState([]);
+
+    // 데이터 받아오기
+    useEffect(() => {
+        fetch("http://localhost:8282/juicyfresh/obtain/list")
+            .then((res) => res.json())
+            .then((res) => {setData(res)})
+    }, []);
+
+    // 엑셀 다운받기 함수
+    const downloadExcel = () => {
+        const wb = XLSX.utils.book_new();
+        const ws = XLSX.utils.table_to_sheet(document.getElementById("dataTable"));
+
+        XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+        const excelBuffer = XLSX.write(wb, {
+            bookType: "xlsx",
+            type: "array",
+        });
+
+        const dataBlob = new Blob([excelBuffer], { type: "application/octet-stream" });
+        saveAs(dataBlob, `수주관리_${Date.now()}.xlsx`);
+    };
+
 
   return(
       <>
@@ -35,17 +46,17 @@ function Obtain() {
           <Button>삭제</Button>
           <Button>확정</Button>
           <img className="excel-icon" src={require('../../img/excel.jpeg')}
-
+               onClick={downloadExcel}
           />
       </div>
 
      <div className="outputArea">
-       <Table striped bordered hover>
+       <Table striped bordered hover id="dataTable">
         <thead>
           <tr>
             <th>No.</th>
             <th>수주번호</th>
-            <th>수주날짜</th>
+            <th>수주일</th>
             <th>품목코드</th>
             <th>제품명</th>
             <th>고객사명</th>
@@ -54,19 +65,25 @@ function Obtain() {
             <th>예상납기일</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>230511-001</td>
-            <td>2023-05-11</td>
-            <td>YB-001</td>
-            <td>양배추즙</td>
-            <td>11번가</td>
-            <td>560</td>
-            <td>2023-05-25</td>
-            <td>2023-05-24</td>
-          </tr>
-        </tbody>
+
+           <tbody>
+           {data?.map((item, index) => {
+               return(
+                   <tr key={index}>
+                       <td>{index+1}</td>
+                       <td>{item.obtainId}</td>
+                       <td>{item.obtainDate.split("T")[0]}</td>
+                       <td>{item.item.itemCode}</td>
+                       <td>{item.item.itemName}</td>
+                       <td>{item.customer.customerName}</td>
+                       <td>{item.obtainAmount}</td>
+                       <td>{item.customerRequestDate.split("T")[0]}</td>
+                       <td>{item.expectDate.split("T")[0]}</td>
+                   </tr>
+               )
+           })
+           }
+           </tbody>
        </Table>
       </div>
       </>
